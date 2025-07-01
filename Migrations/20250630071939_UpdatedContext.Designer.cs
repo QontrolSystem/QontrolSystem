06c43058a79d9afe684e672ea14d919c386e028b
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2025-07-01 10:41:32 +0200

    Files modified: Updated DbContext and migrations.

--- Migrations/20250630071939_UpdatedContext.Designer.cs
+++ Migrations/20250630071939_UpdatedContext.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QontrolSystem.Data;
 
@@ -11,9 +12,11 @@ using QontrolSystem.Data;
 namespace QontrolSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250630071939_UpdatedContext")]
+    partial class UpdatedContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
