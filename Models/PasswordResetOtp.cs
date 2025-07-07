03490e5a552766beb9243b97be6149b3c07bc4f1
Nhlakanipho.Radebe <Nhlakanipho.Radebe@INSPDJJJYSY2>
2025-07-07 15:12:39 +0200

    Feature: OTP entity created to save the otp data to the database

---
+++ Models/PasswordResetOtp.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QontrolSystem.Models
+{
+    public class PasswordResetOtp
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string OtpCode { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+
+        public bool IsUsed { get; set; } = false;
+    }
+}
