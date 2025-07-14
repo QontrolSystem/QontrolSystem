3b5714f3badce981e4a6a698950d8229307e52c4
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2025-07-14 08:57:16 +0200

    File Upload: Created models for ticket attachments, feedback and urgency. File Update: Updated the ticket model to include new foreign keys. Updated the DbContext to include new models and seed urgency.

--- Models/Ticket/Ticket.cs
+++ Models/Ticket/Ticket.cs
@@ -10,15 +10,19 @@
         public int TicketCategoryID { get; set; }
         public int TicketStatusID { get; set; }
         public int CreatedBy { get; set; }
-        public int AssignedTo { get; set; }
+        public int? AssignedTo { get; set; }
         public int DepartmentID { get; set; }
+        public int TicketUrgencyID { get; set; }
+        public int? TicketFeedbackID { get; set; } 
+  
 
         // Date Fields
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
         public DateTime? ResolvedAt { get; set; } // Nullable because not every ticket is resolved immediately
 
         // Navigation Properties
+
         public TicketCategory TicketCategory { get; set; }
         public TicketStatus TicketStatus { get; set; }
 
@@ -26,6 +30,9 @@
         public User Assignee { get; set; }   // AssignedTo User
 
         public Department Department { get; set; }
+        public TicketUrgency TicketUrgency { get; set; }
+        public ICollection<TicketAttachment> TicketAttachments { get; set; }
+        public ICollection<TicketFeedback> TicketFeedbacks { get; set; }
     }
 }
 
