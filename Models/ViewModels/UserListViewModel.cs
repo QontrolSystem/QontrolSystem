82c882afb5b1bbe97a6a2a1993dcbf16248d1ee5
Mnqobi Ntereke <LenkaNtereke25@gmail.com>
2025-07-14 20:23:21 +0200

    Pagination Added

---
+++ Models/ViewModels/UserListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using QontrolSystem.Models;
+
+namespace QontrolSystem.Models.ViewModels
+{
+    public class UserListViewModel
+    {
+        public List<User> Users { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public string SearchString { get; set; }
+        public string RoleFilter { get; set; }
+        public string IsActiveFilter { get; set; }
+        public string DepartmentFilter { get; set; }
+    }
+
+}
