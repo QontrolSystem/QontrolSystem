e91203596ba71843c8286b3bc00d6c16f7ba8868
Mnqobi Ntereke <LenkaNtereke25@gmail.com>
2025-07-15 19:06:57 +0200

    UserList API Controller: Edit Delete Details

---
+++ DTO's/UserEditDto.cs
@@ -0,0 +1,15 @@
+﻿namespace QontrolSystem.DTO_s
+{
+    public class UserEditDto
+    {
+        public int UserID { get; set; }  
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public int RoleID { get; set; }
+        public int DepartmentID { get; set; }
+        public bool IsActive { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
