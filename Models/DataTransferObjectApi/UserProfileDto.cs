bca02c2153d071cca4ef245b0b0a8caf7ece5a2f
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2025-07-15 18:33:46 +0200

    File Update: Updated AccountControllerApi to include api methods for getting and updating user profile. File Upload: Added UpdatedUserProfile and UserProfile data transfer objects.

---
+++ Models/DataTransferObjectApi/UserProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace QontrolSystem.Models.DataTransferObjectApi
+{
+    public class UserProfileDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
