a8482eb2288d6530a602abb0b2c25c1f83d2417b
Nhlakanipho <nhlakaniphoradee337@gmail.com>
2025-07-19 17:58:58 +0200

    Refactor: Updated file names on TransferObjectApi folder and name spaces for reference

---
+++ Data/TransferObjectApi/UpdateUserProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace QontrolSystem.Data.TransferObjectApi
+{
+    public class UpdateUserProfile
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
