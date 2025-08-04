bdd18b38d9ff1b8f4036e4d0b098247341e8ea7c
Nhlakanipho <nhlakaniphoradee337@gmail.com>
2025-08-04 15:06:09 +0200

    Feature: Added a search field on the model for search modeling

--- Models/ViewModels/AssignTicket.cs
+++ Models/ViewModels/AssignTicket.cs
@@ -5,6 +5,7 @@
         public int TicketID { get; set; }
         public int? CurrentAssigneeId { get; set; }
         public DateTime? AssignmentDate { get; set; } // you can map this to ResolvedAt or add a new scheduled field if needed
+        public string? SearchTerm { get; set; }
         public List<ManageTechnicians> TeamUsers { get; set; } = new();
     }
 }
