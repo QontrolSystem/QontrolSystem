c2495bf5126ea6bc652b24a548490ac08d94da63
Nhlakanipho <nhlakaniphoradee337@gmail.com>
2025-08-05 13:08:30 +0200

    Feature: Created a view model consisting of assigned and unsassigned field from the ticket model in order for the dashboard view to map through

---
+++ Models/ViewModels/TicketStatusManager.cs
@@ -0,0 +1,10 @@
+﻿using QontrolSystem.Models.Ticket;
+
+namespace QontrolSystem.Models.ViewModels
+{
+    public class TicketStatusManager
+    {
+        public List<Tickets>? AssignedTickets { get; set; }
+        public List<Tickets>? UnassignedTickets { get; set; }
+    }
+}
